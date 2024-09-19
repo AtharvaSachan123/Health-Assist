7228bef3b7b813b5048dc46f6fb614499115100d
AtharvaSachan123 <atharvasachan823@gmail.com>
2024-09-19 16:05:49 +0530

    Card data

--- src/components/DisplayInfo.jsx
+++ src/components/DisplayInfo.jsx
@@ -1,20 +1,91 @@
+import { IconCircleDashedCheck, IconHourglassHigh, IconUserScan } from '@tabler/icons-react';
 import React from 'react'
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 const DisplayInfo = () => {
-    const [metrics, setMetrics] = useState({
-        totalFolder: 0,
-        aiPersonalizedTreatment: 0,
-        totalScreenings:0,
-        completedScreenings:0,
-        pendingScreenings:0,
-        overdueScreenings:0,
-    });
+  const Navigate = useNavigate();
+  const [metrics, setMetrics] = useState({
+    totalFolder: 0,
+    aiPersonalizedTreatment: 0,
+    totalScreenings: 0,
+    completedScreenings: 0,
+    pendingScreenings: 0,
+    overdueScreenings: 0,
+  });
+  const metricsData = [
+    {
+      title: "Specialist Appointments Pending",
+      subtitle: "View Details",
+      value: metrics.pendingScreenings,
+      icon: IconHourglassHigh,
+      onclick: () => {
+        Navigate('/appointments/pending');
+      }
 
-    
-  return (
-    <>
-    </>
-  )
+    },
+    {
+      title: "Treatment Progress Updates",
+      subtitle: "View Details",
+      value: `${metrics.completedScreenings} of ${metrics.totalScreenings}`,
+      icon: IconCircleDashedCheck,
+      onclick: () => {
+        Navigate('/folders');
+      }
+
+    },
+    {
+      title: "Total Folders",
+      subtitle: "View Details",
+      value: metrics.totalFolder,
+      icon: IconCircleDashedCheck,
+      onclick: () => {
+        Navigate('/folders');
+      }
+
+    },
+    {
+      title: "Total Screenings",
+      subtitle: "View Details",
+      value: metrics.totalScreenings,
+      icon: IconUserScan,
+      onclick: () => {
+        Navigate('/screening');
+      }
+
+    },
+    {
+      title: "Completed Screenings",
+      subtitle: "View Details",
+      value: metrics.completedScreenings,
+      icon: IconUserScan,
+      onclick: () => {
+        Navigate('/screening/completed');
+      }
+
+    },
+    {
+      title: "Pending Screenings",
+      subtitle: "View Details",
+      value: metrics.pendingScreenings,
+      icon: IconHourglassHigh,
+      onclick: () => {
+        Navigate('/screening/pending');
+      }
+
+    },
+    {
+      title: "Overdue Screenings",
+      subtitle: "View Details",
+      value: metrics.overdueScreenings,
+      icon: IconUserScan,
+      onclick: () => {
+        Navigate('/screening/overdue');
+      }
+
+    },
+  ];
+
+
 }
 
 export default DisplayInfo
