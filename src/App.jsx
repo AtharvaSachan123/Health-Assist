e6b417c2876f56662c1685887eaf83e2fe2eb61d
AtharvaSachan123 <atharvasachan823@gmail.com>
2024-09-22 17:48:22 +0530

    Onboarding

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import SideBar from './components/SideBar';
 import Navbar from './components/Navbar';
 
 import Home from './pages/Home';
+import OnBoarding from './pages/OnBoarding';
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path ='/' element={<Home />} />
+          <Route path ='/onboarding' element={<OnBoarding />} />
         </Routes>
       </div>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,2 @@
-export { default as Home } from './Home.jsx';
+export { default as Home } from "./Home";
+export { default as OnBoarding } from "./OnBoarding";
